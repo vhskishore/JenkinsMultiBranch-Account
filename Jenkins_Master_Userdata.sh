3514d9beb619e5a0907dd26aeff610f48e91a703
vhskishore <hemasivakishore@hotmail.com>
2024-01-10 14:59:37 +0530

    Prerequisited listed

---
+++ Jenkins_Master_Userdata.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+sudo apt update && apt install -y openjdk-17-jdk unzip net-tools jq maven awscli
+# sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+# sudo unzip awscliv2.zip
+# sudo sudo ./aws/install
+sudo curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+sudo curl https://get.docker.com | bash
+sudo usermod -a -G docker ubuntu
+sudo usermod -a -G root ubuntu
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+sudo wget https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_linux_amd64.zip
+sudo unzip terraform_1.6.5_linux_amd64.zip
+sudo rm -rf terraform_1.6.5_linux_amd64.zip
+sudo wget https://releases.hashicorp.com/packer/1.10.0/packer_1.10.0_linux_amd64.zip
+sudo unzip packer_1.10.0_linux_amd64.zip
+sudo rm -rf packer_1.10.0_linux_amd64.zip
+sudo chmod 777 packer terraform
+sudo mv terraform packer /usr/local/bin/
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # JenkinsMultiBranch-Account
+
+Tools:
+- Java JDK-17 (Done)
+- Docker (Done)
+- Terraform (Done)
+- Packer (Done)
+- Ansible 
+- Maven (Done)
+- AWS CLI (Done)
+- Jenkins
+- net-tools (Done)
+- jq (Done)
+- unzip (Done)
+
+Jenkins Plugins:
+- AWS Steps
+- BlueOcean
+
+Two Pipelines:
+1. Infrastructure Pipeline - Packer, Terraform, Ansible to build Docker Swarm 3-Node Cluster.
+2. CICD Pipeline. (SpringPetClinic) Build Java Application with Maven and then copy artifact to Jfrog and create a Docker Image using Dockerfile and Deploy it to Docker Swarm.
